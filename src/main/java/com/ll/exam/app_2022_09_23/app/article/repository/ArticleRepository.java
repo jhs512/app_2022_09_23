29ae5057ef64856d8854ba51f24b58dc709f3ca5
jhs512 <jangka512@gmail.com>
2022-09-22 11:51:20 +0900

    게시물 등록

--- src/main/java/com/ll/exam/app_2022_09_23/app/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/exam/app_2022_09_23/app/article/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package com.ll.exam.app_2022_09_23.app.article.repository;
 
 import com.ll.exam.app_2022_09_23.app.article.dto.Article;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -16,4 +17,15 @@ public interface ArticleRepository {
             </script>
             """)
     List<Article> getArticles();
+
+    @Insert("""
+            <script>
+            INSERT INTO article
+            SET createDate = NOW(),
+            modifyDate = NOW(),
+            subject = #{subject},
+            content = #{content}
+            </script>
+            """)
+    void write(String subject, String content);
 }
