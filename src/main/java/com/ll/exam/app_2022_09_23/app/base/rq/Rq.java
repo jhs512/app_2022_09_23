a4b4176758bb40e82d3419c01fe94af18531a748
jhs512 <jangka512@gmail.com>
2022-09-28 14:07:33 +0900

    session ion 스코프

--- src/main/java/com/ll/exam/app_2022_09_23/app/base/rq/Rq.java
+++ src/main/java/com/ll/exam/app_2022_09_23/app/base/rq/Rq.java
@@ -2,9 +2,10 @@ package com.ll.exam.app_2022_09_23.app.base.rq;
 
 import lombok.Getter;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Component
+@SessionScope
 public class Rq {
     @Getter
     private int count;
