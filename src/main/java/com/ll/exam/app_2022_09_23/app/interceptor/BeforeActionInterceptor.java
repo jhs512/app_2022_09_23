e6ba1a1c1550196bf15c4d1a72a747c977326963
jhs512 <jangka512@gmail.com>
2022-09-28 14:42:54 +0900

    Rq 활용

--- src/main/java/com/ll/exam/app_2022_09_23/app/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/ll/exam/app_2022_09_23/app/interceptor/BeforeActionInterceptor.java
@@ -1,6 +1,8 @@
 package com.ll.exam.app_2022_09_23.app.interceptor;
 
 
+import com.ll.exam.app_2022_09_23.app.base.rq.Rq;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -10,7 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class BeforeActionInterceptor implements HandlerInterceptor {
+    private final Rq rq;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.debug("BeforeActionInterceptor::preHandle 실행됨");
--- src/main/java/com/ll/exam/app_2022_09_23/app/interceptor/NeedToLoginInterceptor.java
+++ src/main/java/com/ll/exam/app_2022_09_23/app/interceptor/NeedToLoginInterceptor.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionBindingEvent;
 
 @Component
 @Slf4j
@@ -20,24 +19,12 @@ public class NeedToLoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.debug("this : " + this);
-        log.debug("NeedToLoginInterceptor::preHandle 실행됨");
-        rq.increaseCount();
-        log.debug("rq : " + rq + ", rq.count : " + rq.getCount());
-
-        HttpSession session = request.getSession();
-
-        Long loginedMemberId = (Long) session.getAttribute("loginedMemberId");
-
-        boolean isLogined = loginedMemberId != null; // 임시
-
-        if (isLogined == false) {
+        if (rq.isLogout()) {
             response.setCharacterEncoding("UTF-8");
             response.setContentType("text/html; charset=UTF-8");
             response.getWriter().append("로그인 후 이용바랍니다.");
 
             return false;
-
         }
 
         return HandlerInterceptor.super.preHandle(request, response, handler);
